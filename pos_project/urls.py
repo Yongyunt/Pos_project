83ee4ee4e415b026ff1b804af7aea998eff09d10
Yongyoot <sanT_T1998@hotmail.co.th>
2025-05-13 14:02:37 +0700

    api path

--- pos_project/urls.py
+++ pos_project/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pos_api/', include('pos_api.urls')), 
+
+      # เส้นทางสำหรับ JWT login/refresh
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('', lambda request: HttpResponse('Welcome to POS API')),  
 ]
